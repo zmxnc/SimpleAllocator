7fa4356d8207f833bdfd09db86b0dab9f0217126
Alessio Larcher <alessiolarcher@gmail.com>
2021-02-18 18:40:31 +0100

    Added base allocator class - preparing for generic allocator class

--- allocator.h
+++ allocator.h
@@ -6,24 +6,20 @@
 #include <new>
 
 
-template <class Object>
-class Allocator
+class Allocator_base
   {
-  // The data cache currently in use
-  char *cache;
-  
   public:
   
   unsigned int cache_size = 2048;
+  Allocator_base() = default;
+  virtual ~Allocator_base() = 0;
+
+  virtual void clear() = 0;
+
+  protected:
   
-  Allocator();
-  ~Allocator();
-
-  template <class ... Args>
-  Object& create (Args ... args);
-  void clear();
-
-  private:
+  // The data cache currently in use
+  char *cache;
   
   void allocate_cache();
   // Start of the memory available for allocations
@@ -37,9 +33,67 @@ class Allocator
   };
 
 
+// This class contains the homogeneous implementation
+// (allows for a single data class)
+template <class Object>
+class Allocator : public Allocator_base
+  {
+  public:
+  Allocator();
+  ~Allocator();
+
+  template <class ... Args>
+  Object& create (Args ... args);
+  void clear() override;
+  };
+
+
+// This is the generic implementation
+// Allows allocation of any class (as long as
+// it fits in the cache_size)
+// Has a slight performance penalty, as it requires to
+// store a pointer to the destructor of each object
+class Generic_allocator : public Allocator_base
+  {
+  public:
+  Generic_allocator();
+  ~Generic_allocator();
+
+  template <class Object, class ... Args>
+  Object& create (Args ... args);
+  void clear() override;
+  };
+
+
+Allocator_base :: ~Allocator_base()
+  {  }
+
+void Allocator_base :: allocate_cache()
+  {
+  auto old    = cache;
+  cache       = (char*) malloc (cache_size);
+  previous()  = old;
+  cursor()    = (char*)&start();
+  end()       = cache + cache_size;
+  }
+
+char*& Allocator_base :: start()
+  { return *((char**)cache + 3); }
+
+char*& Allocator_base :: end()
+  { return *((char**)cache + 2); }
+
+char*& Allocator_base :: previous()
+  { return *((char**)cache + 0); }
+
+char*& Allocator_base :: cursor()
+  { return *((char**)cache + 1); }
+
+
+
 template <class Object>
 Allocator<Object> :: Allocator() :
-  cache (nullptr)
+  Allocator_base()
   { allocate_cache(); }
 
 template <class Object>
@@ -87,32 +141,6 @@ void Allocator<Object> :: clear()
   // cache as well, at a small performance cost)
   cursor() = (char*)&start();
   }
-
-template <class Object>
-void Allocator<Object> :: allocate_cache()
-  {
-  auto old    = cache;
-  cache       = (char*) malloc (cache_size);
-  previous()  = old;
-  cursor()    = (char*)&start();
-  end()       = cache + cache_size;
-  }
-
-template <class Object>
-char*& Allocator<Object> :: start()
-  { return *((char**)cache + 3); }
-
-template <class Object>
-char*& Allocator<Object> :: end()
-  { return *((char**)cache + 2); }
-
-template <class Object>
-char*& Allocator<Object> :: previous()
-  { return *((char**)cache + 0); }
-
-template <class Object>
-char*& Allocator<Object> :: cursor()
-  { return *((char**)cache + 1); }
 
 
 #endif
